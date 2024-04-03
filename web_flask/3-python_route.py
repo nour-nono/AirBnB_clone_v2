7c9b529807ee3d63dd18c0f3a336eb2ca24de906
nono <nour98nour@gmail.com>
2024-04-03 02:18:55 +0200

    task 4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""ths is the first flask app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """this is the index page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """this is the hbnb page"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """this is the c page"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('python/<text>', strict_slashes=False)
+def python(text):
+    """this is the python page"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
