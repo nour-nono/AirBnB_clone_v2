1964721fba8b55cefab870c8990aed1b627be790
nono <nour98nour@gmail.com>
2024-04-04 20:35:47 +0200

    hot fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,24 +6,20 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Display HTML page: (inside the tag BODY) """
-    states = storage.all("State").values()
-    states = sorted(states, key=lambda v: v.name)
-    return render_template("7-states_list.html", states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """ Display HTML page: (inside the tag BODY) """
     cities = storage.all("City").values()
     cities = sorted(cities, key=lambda v: v.name)
     return render_template("8-cities_by_states.html"
-                           , states=states, cities=cities)
+                           , cities=cities)
 
 
 @app.teardown_appcontext
 def close_db(err=None):
     """this is the close db"""
     storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
