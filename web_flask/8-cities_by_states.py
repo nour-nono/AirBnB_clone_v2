a6e6fbe3ba216a3f5e02df9e15abe1d5bc26b7ed
nono <nour98nour@gmail.com>
2024-04-04 20:31:26 +0200

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,25 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/cities_by_states')
-def cities_by_states():
-	pass
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Display HTML page: (inside the tag BODY) """
+    states = storage.all("State").values()
+    states = sorted(states, key=lambda v: v.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    """ Display HTML page: (inside the tag BODY) """
+    cities = storage.all("City").values()
+    cities = sorted(cities, key=lambda v: v.name)
+    return render_template("8-cities_by_states.html"
+                           , states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def close_db(err=None):
+    """this is the close db"""
+    storage.close()
